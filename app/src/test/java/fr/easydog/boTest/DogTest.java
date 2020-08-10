03242243429223297cd418b97e05bf480873e98d
Mathias <mathias.robert@yieloo.com>
2020-08-10 15:28:44 +0200

    Test unit for Class dog Object

---
+++ app/src/test/java/fr/easydog/boTest/DogTest.java
@@ -0,0 +1,26 @@
+package fr.easydog.boTest;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+import fr.easydog.bo.Dog;
+
+public class DogTest {
+
+    private Dog dog;
+
+    @Before
+    public void setUp() {
+        dog = new Dog();
+    }
+
+    @Test
+    public void getId(){
+        int attempt = 1;
+        dog.setId(1);
+        assertEquals(attempt, dog.getId());
+    }
+
+}
