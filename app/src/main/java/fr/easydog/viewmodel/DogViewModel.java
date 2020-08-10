777ff311b2d8f7917bafcb6b5146c0893eb47e7b
Mathias <mathias.robert@yieloo.com>
2020-08-10 17:39:35 +0200

    Added DogViewModel

---
+++ app/src/main/java/fr/easydog/viewmodel/DogViewModel.java
@@ -0,0 +1,13 @@
+package fr.easydog.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class DogViewModel extends AndroidViewModel {
+
+    public DogViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
