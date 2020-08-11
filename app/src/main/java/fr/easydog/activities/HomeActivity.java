e8c517f417fa0924378388c2fd572e8e9c462706
Mathias <mathias.robert@yieloo.com>
2020-08-11 15:33:07 +0200

    Now we can see list of dogs created

--- app/src/main/java/fr/easydog/activities/HomeActivity.java
+++ app/src/main/java/fr/easydog/activities/HomeActivity.java
@@ -4,14 +4,30 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import fr.easydog.R;
+import java.util.List;
 
-public class HomeActivity extends Activity {
+import fr.easydog.R;
+import fr.easydog.activities.adapter.DogAdapter;
+import fr.easydog.bo.Dog;
+import fr.easydog.viewmodel.DogViewModel;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private DogViewModel dogViewModel = null;
+
+    private ListView listDog;
 
     private FloatingActionButton fb;
+
+    List<Dog> dogs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +41,19 @@ public class HomeActivity extends Activity {
             public void onClick(View view) {
                 Intent intent = new Intent(HomeActivity.this, AddActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        listDog = findViewById(R.id.lv_my_dog);
+
+        dogViewModel = new ViewModelProvider(this).get(DogViewModel.class);
+
+        dogViewModel.getObserver().observe(this, new Observer<List<Dog>>() {
+            @Override
+            public void onChanged(List<Dog> dogs)
+            {
+                HomeActivity.this.dogs = dogs;
+                listDog.setAdapter(new DogAdapter(HomeActivity.this,R.layout.dog_list_style, dogs));
             }
         });
     }
