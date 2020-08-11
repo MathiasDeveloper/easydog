110dee6237a28b3dcc05b481541c355cb82505a2
Mathias <mathias.robert@yieloo.com>
2020-08-11 09:59:32 +0200

    Added action on click button add on view

--- app/src/main/java/fr/easydog/activities/HomeActivity.java
+++ app/src/main/java/fr/easydog/activities/HomeActivity.java
@@ -1,14 +1,31 @@
 package fr.easydog.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import fr.easydog.R;
 
 public class HomeActivity extends Activity {
+
+    private FloatingActionButton fb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        fb = findViewById(R.id.btn_add);
+
+        fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, AddActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
