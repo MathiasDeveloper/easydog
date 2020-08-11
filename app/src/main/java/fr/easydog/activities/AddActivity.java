e4c355c9d4e69747bea6c36a5c7d44ea58c930ab
Mathias <mathias.robert@yieloo.com>
2020-08-11 14:46:23 +0200

    Now you can add dog with floating button add on app

--- app/src/main/java/fr/easydog/activities/AddActivity.java
+++ app/src/main/java/fr/easydog/activities/AddActivity.java
@@ -1,16 +1,59 @@
 package fr.easydog.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import fr.easydog.R;
+import fr.easydog.activities.utils.Alert;
+import fr.easydog.bo.Dog;
+import fr.easydog.viewmodel.DogViewModel;
 
 public class AddActivity extends AppCompatActivity {
+
+    private DogViewModel dogViewModel = null;
+
+    private EditText editName;
+    private EditText editDescription;
+    //private EditText editDate;
+    //private Spinner spinnerRace;
+
+    private Dog dog = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        editName = findViewById(R.id.et_name);
+        editDescription = findViewById(R.id.et_description);
+        //editDate = findViewById(R.id.et_date);
+        //spinnerRace = findViewById(R.id.spinner_race);
+        //validate = findViewById(R.id.bt_validate);
+
+        dogViewModel = new ViewModelProvider(this).get(DogViewModel.class);
+    }
+
+    public void onClickValidate(View view){
+        dog = new Dog();
+        dog.setName(editName.getText().toString());
+        dog.setDescription(editDescription.getText().toString());
+        //dog.setDate(editName.getText().toString());
+        //dog.setName(editName.getText().toString());
+
+        Log.i("ACOS","INFO : " + dog.toString());
+
+        dogViewModel.insert(dog);
+
+        Toast.makeText(this, Alert.SUCCESS, Toast.LENGTH_SHORT).show();
+
+        finish();
     }
 }
