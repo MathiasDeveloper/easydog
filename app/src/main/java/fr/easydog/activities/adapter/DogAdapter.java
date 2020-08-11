9b864d29c4da8deaaf7ccdfb886a428dfdb0e3ee
Mathias <mathias.robert@yieloo.com>
2020-08-11 15:32:35 +0200

    Added list style dog for list and new class adapter Dog

---
+++ app/src/main/java/fr/easydog/activities/adapter/DogAdapter.java
@@ -0,0 +1,44 @@
+package fr.easydog.activities.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+import fr.easydog.R;
+import fr.easydog.bo.Dog;
+
+public class DogAdapter extends ArrayAdapter<Dog> {
+
+    public DogAdapter(@NonNull Context context, int resource, @NonNull List objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        if(convertView == null)
+        {
+            LayoutInflater li = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = li.inflate(R.layout.dog_list_style, parent,false);
+        }
+
+
+        TextView textViewName = convertView.findViewById(R.id.tv_name);
+
+        Dog dog = getItem(position);
+
+        textViewName.setText(dog.getName());
+
+        return convertView;
+    }
+}
