b37df23598ce0207f26074ae3e9ffd1dd582297f
Mathias <mathias.robert@yieloo.com>
2020-08-12 10:26:10 +0200

    Added activity for race object

---
+++ app/src/main/java/fr/easydog/activities/race/AddRaceActivity.java
@@ -0,0 +1,16 @@
+package fr.easydog.activities.race;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fr.easydog.R;
+
+public class AddRaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_race);
+    }
+}
