5061a2cd407d85eff6c3810557faa995ba044a67
Mathias <mathias.robert@yieloo.com>
2020-08-12 10:21:04 +0200

    Added new message in Alert class

--- app/src/main/java/fr/easydog/activities/utils/Alert.java
+++ app/src/main/java/fr/easydog/activities/utils/Alert.java
@@ -2,4 +2,5 @@ package fr.easydog.activities.utils;
 
 public class Alert {
      public static final String SUCCESS  = "Succès";
+     public static final String UPDATE_SUCCESS  = "Modification faite avec succès !";
 }
