0528f64decf56b66b2612f272dd03f506e9fe18c
Mathias <mathias.robert35@hotmail.fr>
2020-08-12 18:30:02 +0200

    Added logic for view model race

--- app/src/main/java/fr/easydog/viewmodel/RaceViewModel.java
+++ app/src/main/java/fr/easydog/viewmodel/RaceViewModel.java
@@ -1,4 +1,46 @@
 package fr.easydog.viewmodel;
 
-public class RaceViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import fr.easydog.bo.Race;
+import fr.easydog.repositories.race.RaceRepository;
+import fr.easydog.repositories.race.RaceRepositoryInterface;
+
+public class RaceViewModel extends AndroidViewModel {
+
+    private RaceRepositoryInterface raceRepository;
+
+    public RaceViewModel(@NonNull Application application) {
+        super(application);
+        raceRepository = new RaceRepository(application);
+    }
+
+    public LiveData<List<Race>> getObserver(){
+        return raceRepository.getObserver();
+    }
+    public LiveData<List<String>> getObserverLabel(){
+        return raceRepository.getObserverLabel();
+    }
+
+    public void insert(final Race race) {
+        raceRepository.insert(race);
+    }
+    public void getById(int id) {
+        raceRepository.getById(id);
+    }
+    public void getAll() {
+        raceRepository.getAll();
+    }
+    public void update(final Race race) {
+        raceRepository.update(race);
+    }
+    public void delete(final Race race) {
+        raceRepository.delete(race);
+    }
 }
