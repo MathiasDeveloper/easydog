60909b9e26c477c4bcdd0e3d0c074371c8728f37
Mathias <mathias.robert35@hotmail.fr>
2020-08-12 12:25:58 +0200

    Added room fixture

--- app/src/main/java/fr/easydog/dao/utils/AppDatabase.java
+++ app/src/main/java/fr/easydog/dao/utils/AppDatabase.java
@@ -1,22 +1,27 @@
 package fr.easydog.dao.utils;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import fr.easydog.bo.Dog;
+import fr.easydog.bo.Race;
 import fr.easydog.bo.converter.DateConverter;
 import fr.easydog.bo.converter.RaceConverter;
 import fr.easydog.config.Environment;
 import fr.easydog.dao.DogDaoInterface;
+import fr.easydog.dao.RaceDaoInterface;
 
-@Database(entities = {Dog.class}, version = Environment.VERSION, exportSchema = false)
+@Database(entities = {Dog.class, Race.class}, version = Environment.VERSION, exportSchema = false)
 @TypeConverters({DateConverter.class, RaceConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
 
@@ -24,7 +29,9 @@ public abstract class AppDatabase extends RoomDatabase {
 
     public static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(1);
 
-    public abstract DogDaoInterface dogDao();
+    public abstract DogDaoInterface getDogDao();
+
+    public abstract RaceDaoInterface getRaceDao();
 
     public static AppDatabase getInstance(Context context) {
         if (null == INSTANCE) {
@@ -34,10 +41,41 @@ public abstract class AppDatabase extends RoomDatabase {
                             context,
                             AppDatabase.class,
                             Environment.DB_NAME)
+                            .addCallback(roomFixture)
+                            .fallbackToDestructiveMigration()
                             .build();
                 }
             }
         }
         return INSTANCE;
     }
+
+    private static Callback roomFixture = new Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new AsyncTask<AppDatabase,Void,Void>() {
+                @Override
+                protected Void doInBackground(AppDatabase... appDatabases) {
+                    DogDaoInterface dogDao = INSTANCE.getDogDao();
+                    RaceDaoInterface raceDao = INSTANCE.getRaceDao();
+                    dogDao.insert(new Dog(
+                            0,
+                            "Joyce",
+                            "Un super chien qui adore les pates",
+                            null,
+                            null ));
+                    dogDao.insert(new Dog(
+                            0,
+                            "Tom",
+                            "Un super chien qui cherche des amies",
+                            null,
+                            null));
+                    raceDao.insert(new Race("Pitbull"));
+                    raceDao.insert(new Race("Staff"));
+                    return null;
+                }
+            }.execute(INSTANCE);
+        }
+    };
 }
