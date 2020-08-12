fb6c566e01d454d505931c28a3f338388c01eebd
Mathias <mathias.robert35@hotmail.fr>
2020-08-12 15:14:58 +0200

    Added alert for like and dislike

--- app/src/main/java/fr/easydog/activities/utils/Alert.java
+++ app/src/main/java/fr/easydog/activities/utils/Alert.java
@@ -1,6 +1,8 @@
 package fr.easydog.activities.utils;
 
 public class Alert {
-     public static final String SUCCESS  = "Succès";
-     public static final String UPDATE_SUCCESS  = "Modification faite avec succès !";
+    public static final String SUCCESS = "Succès";
+    public static final String UPDATE_SUCCESS = "Modification faite avec succès !";
+    public static final String LIKE = "Tu as liker ce chien, vas-tu matcher avec le propriétaire ???";
+    public static final String DISLIKE = "Dommage, tu as disliké le chien... le proprio boude !";
 }
